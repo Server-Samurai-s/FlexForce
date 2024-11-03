6547926df6c518081ae30b75deee138ccf08cce6
st10082679 <st10082679@vcconnect.edu.za>
2024-11-03 14:28:18 +0200

    Re-add FlexForce as a regular directory

--- FlexForce/app/src/main/java/za/co/varsitycollege/serversamurai/flexforce/MuscleGroupItem.kt
+++
@@ -1,7 +0,0 @@
-package za.co.varsitycollege.serversamurai.flexforce
-
-data class MuscleGroupItem(
-    val name: String,
-    val icon: Int,  // Drawable resource ID
-    var isSelected: Boolean = false  // Keeps track of selection status
-)
